b41719fa1ff6d44baa04d3bff48e4a2bddf19cae
Nick Albu <nicolasmalbu@gmail.com>
2021-07-15 17:21:39 -0400

    setup initial folder structure for HomePage components

---
+++ client/src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import './HomePage.scss';
+import { Component } from 'react';
+
+class HomePage extends Component {
+
+    render () {
+        return (
+            <section>
+                <h1>Nick Albu's Portfolio</h1>
+            </section>
+            
+        )
+    }
+}
+
+export default HomePage;
