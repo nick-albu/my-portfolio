b2c655b2e7fc2798f67c1c950f77299d42def528
Nick Albu <nicolasmalbu@gmail.com>
2021-07-21 19:21:31 -0400

    added Gemini to Projects component

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,5 +1,10 @@
 import './Projects.scss';
 import { Component } from 'react';
+import GeminiLogo from '../../assets/GeminiLogo.svg';
+import GeminiLanding from '../../assets/GeminiLandingPage.PNG';
+import GeminiCreate from '../../assets/GeminiCreatePage.PNG';
+import GeminiPrevious from '../../assets/GeminiPreviousWorkouts.PNG';
+import GeminiWorkout from '../../assets/GeminiWorkoutPage.PNG';
 
 class Projects extends Component {
 
@@ -7,9 +12,17 @@ class Projects extends Component {
         return (
             <section className='projects'>
                 <h1 className='projects__header'>My Projects</h1>
-                <p className='projects__body'>My first major project was my capstone assignment
-                at BrainStation. Gemini is a user friendly fitness tracking app. Gemini uses JavaScript, React.js, 
-                Scss, Express.js and MongoDB. </p>
+                <div className='projects__itemContainer'>
+                <img className='projects__itemLogo' src={GeminiLogo} alt='gemini-logo'/>
+                    <div className='projects__imageContainer'>
+                        <img className='projects__itemImage' src={GeminiLanding} alt='gemini-landing'/>
+                        <img className='projects__itemImage' src={GeminiCreate} alt='gemini-landing'/>
+                        <img className='projects__itemImage' src={GeminiPrevious} alt='gemini-landing'/>
+                        <img className='projects__itemImage' src={GeminiWorkout} alt='gemini-landing'/>
+                    </div>
+                    <p className='projects__itemBody'>Gemini is a user friendly fitness tracking app. 
+                    Gemini uses JavaScript, React.js, Scss, Express.js and MongoDB. </p>
+                    </div>
             </section>
         )
     }
