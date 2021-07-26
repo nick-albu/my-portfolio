d0774d9fbdeb34819fc19372d5051849410832a3
Nick Albu <nicolasmalbu@gmail.com>
2021-07-25 23:43:12 -0400

    icons imported into Contact.js

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,11 +1,19 @@
 import './Contact.scss';
 import { Component } from 'react';
+import Email from '../../assets/EmailIcon.png'
+import GitHub from '../../assets/GitHubIcon.png'
+import LinkedIn from '../../assets/LinkedInIcon.png'
 
 class Contact extends Component {
 
     render () {
         return (
-            <h1>Contact Section</h1>
+            <section>
+                <a href=''><img src={LinkedIn} alt='linkedin'/></a>
+                <a href=''><img src={GitHub} alt='github'/></a>
+                <a href=''><img src={LinkedIn} alt='email'/></a>
+            </section>
+            
         )
     }
 }
