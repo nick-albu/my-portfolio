4c21da74f22f3b161be424560df95da76e78b42b
Nick Albu <nicolasmalbu@gmail.com>
2021-07-28 17:03:17 -0400

    techstack header styling, project hover testing in Projects.scss, changed Contact background color

--- client/src/components/TechStack/TechStack.js
+++ client/src/components/TechStack/TechStack.js
@@ -1,18 +1,26 @@
 import './TechStack.scss'
 import { Component } from 'react';
+import JavaScript from '../../assets/TechStackIcons/javascript.svg'
+import Expressjs from '../../assets/TechStackIcons/express.svg'
+import HTML5 from '../../assets/TechStackIcons/html5.svg'
+import Mongo from '../../assets/TechStackIcons/mongodb.svg'
+import Reactjs from '../../assets/TechStackIcons/react.svg'
+import Sass from '../../assets/TechStackIcons/sass.svg'
 
 class TechStack extends Component {
     render () {
         return (
             <section className='techStack'>
-                <h1 className='techStack__header'>Languages I've Learned</h1>
+                <div className='techStack__headerContainer'>
+                    <h1 className='techStack__header'>Languages I've Learned</h1>
+                </div>
                 <ul className='techStack__list'>
-                    <li className='techStack__item'>JavaScript<img /></li>
-                    <li className='techStack__item'>React.js<img /></li>
-                    <li className='techStack__item'>Express.js<img /></li>
-                    <li className='techStack__item'>MongoDB<img /></li>
-                    <li className='techStack__item'>Scss<img /></li>
-                    <li className='techStack__item'>HTML 5<img /></li>
+                    <li className='techStack__item'>JavaScript<img className='techStack__icon' src={JavaScript}/></li>
+                    <li className='techStack__item'>React.js<img className='techStack__icon' src={Reactjs}/></li>
+                    <li className='techStack__item'>Express.js<img className='techStack__iconLarge' src={Expressjs}/></li>
+                    <li className='techStack__item'>MongoDB<img className='techStack__icon' src={Mongo}/></li>
+                    <li className='techStack__item'>Sass<img className='techStack__icon' src={Sass}/></li>
+                    <li className='techStack__item'>HTML 5<img className='techStack__icon' src={HTML5}/></li>
                 </ul>
             </section>
         )
