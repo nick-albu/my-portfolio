41dd414d9a26e4ea42eefa9c2f6d188166f36edc
Nick Albu <nicolasmalbu@gmail.com>
2021-07-29 23:55:12 -0400

    setup initial Contact.js header JSX

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -9,9 +9,20 @@ class Contact extends Component {
     render () {
         return (
             <section className='contact'>
-                <a href='https://www.linkedin.com/in/nicolasalbu'><img className='contact__icon' src={LinkedIn} alt='linkedin'/></a>
-                <a href=''><img className='contact__emailIcon' src={Email} alt='email'/></a>
-                <a href='https://github.com/nick-albu'><img className='contact__icon' src={GitHub} alt='github'/></a>
+                <div className='contact__headerContainer'>
+                        <div className='contact__headerBackLeft'>
+                            <div className='contact__headerFrontLeft'>
+                            </div>
+                        </div>
+                        <div className='contact__headerBackRight'>
+                            <h1 className='contact__header'>Contact Me</h1>
+                        </div>
+                </div>
+                <div className='contact__footer'>
+                    <a href='https://www.linkedin.com/in/nicolasalbu'><img className='contact__icon' src={LinkedIn} alt='linkedin'/></a>
+                    <a href=''><img className='contact__emailIcon' src={Email} alt='email'/></a>
+                    <a href='https://github.com/nick-albu'><img className='contact__icon' src={GitHub} alt='github'/></a>
+                </div>
             </section>
             
         )
