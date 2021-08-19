8b154fc1c2b4bbacc4f9d533ec46ce0e2c447af9
Nick Albu <nicolasmalbu@gmail.com>
2021-08-19 12:39:15 -0400

    fixed a grammer error in About.js

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -24,8 +24,8 @@ class About extends Component {
                 </div>
                 
                 <div className='about__bodyContainer'>
-                    <p className='about__body'>I love developing. It let's me be creative
-                    and solve problems at the same time, it also let's me love what I do every day.</p>
+                    <p className='about__body'>I love developing. It lets me be creative
+                    and solve problems at the same time, it also means I love what I do every day.</p>
                     <p className='about__body'>I've recently made a career change
                     into web development. After graduating at BrainStation's Web Development program
                     I'm currently looking for a Junior Developer role and working on my own projects.</p>
