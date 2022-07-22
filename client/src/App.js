5e7f990cb2bfabddd6ac950d289d3c9b0c159496
nick-albu <nicolasmalbu@gmail.com>
2022-07-22 09:55:02 -0400

    removed Switch/new version of react-router and Switch is unecessary anyways

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import './App.scss';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage/HomePage'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Switch>
-          <Route path='/' exact component={HomePage} />
-        </Switch>
-      </BrowserRouter>
+      <HomePage />
     </>
   );
 }
